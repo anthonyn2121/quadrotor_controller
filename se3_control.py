3f0a4ea530aae4e906369be8d0c6d4a1bc42dc86
anthonyn2121 <50376738+anthonyn2121@users.noreply.github.com>
2020-12-01 13:20:03 -0500

    Add files via upload

--- se3_control.py
+++ se3_control.py
@@ -1,6 +1,15 @@
 import numpy as np
 from scipy.spatial.transform import Rotation
-from math import cos, sin
+
+def S(x): 
+    return np.sin(x)
+
+def C(x): 
+    return np.cos(x)
+
+def T(x):
+    return np.tan(x)
+
 
 
 class SE3Control(object):
@@ -36,15 +45,18 @@ class SE3Control(object):
         # You may define any additional constants you like including control gains.
         self.inertia = np.diag(np.array([self.Ixx, self.Iyy, self.Izz])) # kg*m^2 body inertia 
         self.g = 9.81 # m/s^2
-
+        self.ct = self.k_thrust
+        self.cq = self.k_drag
+        self.m = self.mass
+        self.l = self.arm_length
+        self.gamma = self.k_drag/self.k_thrust
+        
         # STUDENT CODE HERE
-        self.Kp = np.diag(np.array([1,1,1]))
-        self.Kd = np.diag(np.array([1,1,1]))
-        self.Kr = np.diag(np.array([250, 250, 250]))
-        self.Kw = np.diag(np.array([20, 20, 20]))
-        self.grav = np.array([0,0,-self.mass * self.g])
-        self.d = self.k_drag/self.k_thrust
-
+        self.Kp = np.diag(np.array([2, 2, 2]))
+        self.Kd = np.diag(np.array([15, 30, 10]))
+        self.Kr = np.diag(np.array([3, 3, 10]))
+        self.Kw = np.diag(np.array([20, 40, 10]))
+        
         
     def update(self, t, state, flat_output):
         """
@@ -78,7 +90,6 @@ class SE3Control(object):
         cmd_thrust = 0
         cmd_moment = np.zeros((3,))
         cmd_q = np.zeros((4,))
-
         # STUDENT CODE HERE
         ''' 
         inner controller STATE = [roll, pitch, yaw, roll rate, pitch rate, yaw rate]
@@ -86,8 +97,43 @@ class SE3Control(object):
             roll, pitch, yaw RATES given from: state['w']
         ''' 
 
-        (f1, f2, f3, f4) = self.get_forces(state, flat_output)
-        print(f1)
+        # Compute the commanded acceleraton r_ddot_des 
+        r_ddot_des = flat_output['x_ddot'] - self.Kd @ (state['v'] - flat_output['x_dot']) - self.Kp @ (state['x'] - flat_output['x'])
+        
+        # Compute the desired angles phi_des, theta_des from the desired acceleration 
+        phi_des = (S(flat_output['yaw'])*r_ddot_des[0] - r_ddot_des[1]*C(flat_output['yaw']))/self.g  # roll 
+        theta_des = (S(flat_output['yaw'])*r_ddot_des[0] + r_ddot_des[1]*C(flat_output['yaw']))/self.g # pitch
+        psi_des = flat_output['yaw']
+        euler_des = np.array([phi_des, theta_des, psi_des])
+
+        # Find the total upward thrust from equation of motion 
+        u1 = (r_ddot_des[2] + self.g) * self.mass
+        
+        # Find the moment vector 
+        euler_ang = Rotation.from_quat(state['q']).as_euler('xyz', degrees=False)
+        omega_des = np.array([0, 0, flat_output['yaw_dot']])
+        u2  = self.inertia @ (-self.Kr @ (euler_ang - euler_des) - self.Kw @ (state['w'] - omega_des))
+        
+        # From sys inputs, find real inputs 
+        gamma = self.k_drag/self.k_thrust
+        l = self.arm_length 
+        A = np.array([
+            [1, 1, 1, 1], 
+            [0, l, 0, -l], 
+            [-l, 0, l, 0], 
+            [gamma, -gamma, gamma, -gamma]
+        ])
+        u = np.append(u1, u2)
+        
+        F = np.linalg.inv(A) @ u 
+        
+        cmd_motor_speeds = np.sqrt(F/self.k_thrust)
+
+        for i in range(len(cmd_motor_speeds)): 
+            if cmd_motor_speeds[i] < self.rotor_speed_min: 
+                cmd_motor_speeds[i] = self.rotor_speed_min
+            if cmd_motor_speeds[i] > self.rotor_speed_max: 
+                cmd_motor_speeds[i] = self.rotor_speed_max
 
         control_input = {'cmd_motor_speeds':cmd_motor_speeds,
                          'cmd_thrust':cmd_thrust,
@@ -95,37 +141,3 @@ class SE3Control(object):
                          'cmd_q':cmd_q}
         return control_input
 
-
-
-    def get_forces(self, state, flat_output): 
-        quat = Rotation.from_quat(state['q'])
-        euler = quat.as_euler('zyx', degrees=False)
-        R = quat.as_matrix()
-        
-        r_des = flat_output['x_ddot'] + -self.Kd @ (state['v'] - flat_output['x_dot']) + -self.Kp @ (state['x']-flat_output['x'])
-        F_des = self.mass*r_des + self.grav 
-
-        b3_des = F_des/np.linalg.norm(F_des) # align b3 body axis so that it is oriented along desried thrust
-        a_psi = np.asarray([np.cos(flat_output['yaw']), np.sin(flat_output['yaw']), 0])
-        b2_des = np.cross(b3_des, a_psi)/np.linalg.norm(np.cross(b3_des, a_psi))
-        b1_des = np.cross(b2_des, b3_des)        
-        R_des = np.concatenate((b1_des.reshape((3,1)), b2_des.reshape((3,1)), b3_des.reshape((3,1))), axis=1)
-        
-        eR = .5* (R_des.T @ R - R.T @ R_des)
-        eR = np.array([eR[2, 1], eR[0, 2], eR[1, 0]])
-        eR = eR.reshape((3,1))
-        w_des = np.zeros((3))
-        eW = state['w'] - w_des
-        eW = eW.reshape((3,1))
-        u2 = np.matmul(self.inertia,(np.matmul(-self.Kr,eR) - np.matmul(self.Kw,eW)))
-        u1 = (R @ np.array([0,0,1]).reshape((3,1))).T @ F_des
-
-        u = (np.vstack((u1,u2)))
-        
-        F = np.linalg.inv(np.array([[1,1,1,1],
-                    [0, self.arm_length, 0, -self.arm_length],
-                    [-self.arm_length, 0, self.arm_length, 0],
-                    [self.d, -self.d, self.d, -self.d]]))   @ u
-        print(F)
-        # print(F)
-        return F
